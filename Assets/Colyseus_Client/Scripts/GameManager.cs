880b6df158036717a30e6207d64c65aaf0541846
ThunderVolt45 <92630716+ThunderVolt45@users.noreply.github.com>
2025-07-15 00:56:29 +0900

    Create GameManager.cs

---
+++ Assets/Colyseus_Client/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+        Application.targetFrameRate = (int)Screen.currentResolution.refreshRateRatio.value;
+    }
+}
